246fce29746015ed09e2e313640c18c5e6eec1d0
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-11 11:01:47 -0800

    updated mach for email not user

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Email must match'],
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Email must match'],
     },
     thoughts: [
       {
