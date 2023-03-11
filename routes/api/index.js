86fe906008fc84a1569d4c2376c35a143f07c71b
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-11 11:11:10 -0800

    fixed routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 //routes to use in local/api/...
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
