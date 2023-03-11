a8b15c53ff1640f0bd2796284c8ba2a4aa3ddc64
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-11 07:53:33 -0800

    deleted utils folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-
+//routes to use in local/api/...
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
